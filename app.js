4d4d7a2869dcf4e573a316a5018a6a60f4885b02
YH4-ES <yifanh4@nycstudents.net>
2025-10-31 14:00:13 -0400

    asdf

---
+++ app.js
@@ -0,0 +1,16 @@
+
+function addList(){
+    document.querySelector(".playlistcontainer").insertAdjacentHTML(
+    "beforeend",
+    `<div class="playlistitem">New Item</div>`);
+};
+addList();
+addList();
+addList();
+addList();
+addList();
+addList();
+addList();
+addList();
+addList();
+addList();
