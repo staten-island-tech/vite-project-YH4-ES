6f1fa32433f1dad5ab5b2782a63b1465744902b2
YH4-ES <yifanh4@nycstudents.net>
2025-11-03 13:41:27 -0500

    sdf

--- app.js
+++ app.js
@@ -2,7 +2,11 @@
 function addList(){
     document.querySelector(".playlistcontainer").insertAdjacentHTML(
     "beforeend",
-    `<div class="playlistitem">New Item</div>`);
+    `<div class="playlistitem">
+        <img class="playlistimg" src="https://images.genius.com/65d1deeee872f584f50b39dfa073245e.1000x1000x1.jpg" alt="Playlist Image">
+        <div class="psongcontainer"></div>
+    </div>`
+);
 };
 addList();
 addList();
