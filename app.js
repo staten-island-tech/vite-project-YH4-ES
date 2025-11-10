5ae42ff20f47478632fe3b2150ff4e344eadfbda
YH4-ES <yifanh4@nycstudents.net>
2025-11-10 13:24:15 -0500

    sdfg

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ function addList(){
     document.querySelector(".playlistcontainer").insertAdjacentHTML(
     "beforeend",
     `<div class="playlistitem">
-        <img class="playlistimg" src="https://images.genius.com/65d1deeee872f584f50b39dfa073245e.1000x1000x1.jpg" alt="Playlist Image">
+        <img class="playlistimg" src="https://t2.genius.com/unsafe/300x300/https%3A%2F%2Fimages.genius.com%2F65d1deeee872f584f50b39dfa073245e.1000x1000x1.jpg" alt="Playlist Image">
         <div class="psongcontainer"></div>
     </div>`
 );
