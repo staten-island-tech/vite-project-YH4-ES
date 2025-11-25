778a57acb26698343cec58190a4478b00282907a
YH4-ES <yifanh4@nycstudents.net>
2025-11-25 13:55:34 -0500

    asdf

--- app.js
+++ app.js
@@ -8,43 +8,35 @@ function addList(){
     </div>`
 );
 };
-function addSong(){
+function addSong(title, song){
     document.querySelector(".psongcontainer").insertAdjacentHTML(
     "beforeend",
     `<div class = "songbar">
         </img class="play" scr="">
         </img class="stop" scr="">
-        <p class = "bartext">"XYZ SONG"</p>
+        <p class = "bartext">${title}</p>
+        <audio id = "song" autostart = "false" src = "${song}" loop ></audio>
     </div> `
     )
 }
 const sample = document.querySelectorAll(".sampleitem");
 const sampleArray = Array.from(sample);
-let title = ""
+let data = {
+    title: "",
+    song: ""
+}
 function enableSelect(){
     sample.forEach((item) => {
         item.addEventListener("click", () => {
-        title = item.getAttribute("data-title")
-        console.log(title)
-        console.log("john", sampleArray.indexOf(item), title)
-        return title;
+        data.title = item.getAttribute("data-title")
+        data.song = item.getAttribute("data-song")
+        console.log(data.title)
+        console.log("john", sampleArray.indexOf(item), data.title)
+        return data
     });
     });
 };
 let titlekey ="";
-const container = document.querySelectorAll(".playlistitem");
-console.log(container, "contain");
-function inputSelect(){
-    container.forEach((item)=> {
-        item.addEventListener("click", () => {
-            titlekey = Object.keys(title);
-            console.log(titlekey)
-            if (titlekey.length != 0) {
-                console.log("boingo")
-            };
-        });
-    });
-};
 
 
 console.log(sample)
@@ -63,5 +55,20 @@ addSong();
 addSong();
 addSong();
 addSong();
+
+const container = document.querySelectorAll(".playlistitem");
+console.log(container, "contain");
+function inputSelect(){
+    container.forEach((item)=> {
+        item.addEventListener("click", () => {
+            titlekey = Object.keys(data.title);
+            console.log(titlekey)
+            if (titlekey.length != 0) {
+                console.log(data.title)
+                addSong(data.title, data.song)
+            };
+        });
+    });
+};
 enableSelect();
 inputSelect();
