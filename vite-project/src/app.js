92c8dca48219b84110e1129ca28d9f7cff0c01a9
YH4-ES <yifanh4@nycstudents.net>
2025-12-02 14:01:24 -0500

    zxcv

--- vite-project/src/app.js
+++ vite-project/src/app.js
@@ -4,7 +4,7 @@ let i =0
 function addList(){
     document.querySelector(".playlistcontainer").insertAdjacentHTML(
     "beforeend",
-    `<div class="playlistitem">
+    `<div class="playlistitem" data-playlist0 = "${i}">
         <img class="playlistimg" src="https://t2.genius.com/unsafe/300x300/https%3A%2F%2Fimages.genius.com%2F65d1deeee872f584f50b39dfa073245e.1000x1000x1.jpg" alt="Playlist Image">
         <div class="psongcontainer" data-playlist = "${i}"></div>
     </div>`
@@ -12,8 +12,9 @@ function addList(){
     );
 i++
 };
+let selectedContainer = i
 function addSong(title, song){
-    document.querySelector("[data-playlist = '0']").insertAdjacentHTML( //I'm sure this is the worst method possible but i am very proud
+    document.querySelector(`[data-playlist = '${selectedContainer}']`).insertAdjacentHTML( //I'm sure this is the worst method possible but i am very proud
     "beforeend",
     `<div class = "songbar">
         </img class="play" scr="">
@@ -21,7 +22,9 @@ function addSong(title, song){
         <p class = "bartext">${title}</p>
         <audio id = "song" autostart = "false" src = "${song}" loop ></audio>
     </div> `
-    )
+    
+    );
+    console.log(document.getElementById("id"))
 }
 const sample = document.querySelectorAll(".sampleitem");
 const sampleArray = Array.from(sample);
@@ -55,12 +58,9 @@ addList();
 addList();
 addList();
 addSong();
-addSong();
-addSong();
-addSong();
-addSong();
 
 const container = document.querySelectorAll(".playlistitem");
+
 console.log(container, "contain");
 function inputSelect(){
     container.forEach((item)=> {
@@ -69,6 +69,9 @@ function inputSelect(){
             console.log(titlekey)
             if (titlekey.length != 0) {
                 console.log(data.title)
+                console.log(data.song)
+                console.log(item.getAttribute("data-playlist0"))
+                selectedContainer = item.getAttribute("data-playlist0")
                 addSong(data.title, data.song)
             };
         });
